aa742e0e55eda7c5ed2fea5cbda48183b1a1f4e4
Mickaël Bernardini <contact@mickael-bernardini.fr>
2017-01-25 23:29:14 +0100

    version du soir

--- tp2.c
+++ tp2.c
@@ -14,6 +14,52 @@ typedef struct soluce {
     float value;
 } soluce;
 
+int getNbObject(FILE* f) {
+    int nb_object = 0;
+    do {
+        if (fgetc(f) == '\n') nb_object ++;
+    } while (! feof(f));
+    return nb_object;
+}
+
+int getMaxWeight(FILE* f) {
+    rewind(f);
+    int max_weight;
+    fscanf(f, "%d", &max_weight);
+    return max_weight;
+}
+
+void fillObjects(FILE* f, object** objects) {
+    float weight, value;
+    int i = 0;
+    do {
+        fscanf(f, "%f %f", &weight, &value);
+        object* obj = (object*) malloc(sizeof(object));
+        obj->name = i;
+        obj->weight = weight;
+        obj->value = value;
+        obj->ratio = value / weight;
+        objects[i] = obj;
+        ++i;
+    } while (! feof(f));
+}
+
+void sortObjects(object** objects, int nb_object) {
+    int j, i = 0;
+    while(i < nb_object - 1) {
+        j = nb_object - 1;
+        while(j > i) {
+            if (objects[i]->ratio < objects[j]->ratio) {
+                object* tmp = objects[i];
+                objects[i] = objects[j];
+                objects[j] = tmp;
+            }
+            --j;
+        }
+        i++;
+    }
+}
+
 void displayObject(object* object) {
     printf("%d\tpoid: %f\tvaleur: %f\tratio: %f\n",
         object->name, object->weight, object->value, object->ratio);
@@ -46,7 +92,10 @@ float branchAndBound(object** objects, int level, int nb_object, float weight, f
         s1 = branchAndBound(objects, level + 1, nb_object, weight - objects[level]->weight, current_value + objects[level]->value, nb_node);
     }
 
-    float s2 = branchAndBound(objects, level + 1, nb_object, weight, current_value, nb_node);
+    float s2 = -10000;
+    if (1) {
+        s2 = branchAndBound(objects, level + 1, nb_object, weight, current_value, nb_node);
+    }
 
     if (s1 > s2) return s1;
     return s2;
@@ -58,61 +107,24 @@ int main(int argc, char const *argv[]) {
 
     // REMPLIR --------------------------------------
     FILE* f = fopen(argv[1], "r");
-    int nb_object = 0;
-    char c;
-    do {
-        c = fgetc(f);
-        if (c == '\n') nb_object ++;
-    } while (! feof(f));
+    int nb_object = getNbObject(f);
+    object** objects = (object**) malloc(nb_object * sizeof(object*));
+    int max_weight = getMaxWeight(f);
+    fillObjects(f, objects);
+    fclose(f);
     printf("nb_object : %d\n", nb_object);
 
-    object** objects = (object**) malloc(nb_object * sizeof(object*));
+    // TRIER (bulle :| ) --------------------------------------
+    sortObjects(objects, nb_object);
+    for (int i = 0; i < nb_object; ++i) displayObject(objects[i]);
 
-    rewind(f);
-    float weight, value;
-    int max_weight, i = 0;
-    fscanf(f, "%d", &max_weight);
-    do {
-        fscanf(f, "%f %f", &weight, &value);
-        object* obj = (object*) malloc(sizeof(object));
-        obj->name = i;
-        obj->weight = weight;
-        obj->value = value;
-        obj->ratio = value / weight;
-        objects[i] = obj;
-        ++i;
-    } while (! feof(f));
-    fclose(f);
-    // SOLVE --------------------------------------
-
-    // max (sum(value * is_taken))
-    // avec sum(weight * is_taken) <= max_weight
-
-    // trier le tableau (tri bulle :| )
-    int j;
-    i = 0;
-    while(i < nb_object - 1) {
-        j = nb_object - 1;
-        while(j > i) {
-            if (objects[i]->ratio < objects[j]->ratio) {
-                object* tmp = objects[i];
-                objects[i] = objects[j];
-                objects[j] = tmp;
-            }
-            --j;
-        }
-        i++;
-    }
-
-    for (i = 0; i < nb_object; ++i)
-        displayObject(objects[i]);
-
+    // RESOUDRE --------------------------------------
     int nb_node = 0;
     float bnb = branchAndBound(objects, 0, nb_object, max_weight, 0, &nb_node);
     printf("optimum: %f node: %d\n", bnb, nb_node);
 
     // FREE --------------------------------------
-    for (i = 0; i < nb_object; ++i) free(objects[i]);
+    for (int i = 0; i < nb_object; ++i) free(objects[i]);
     free(objects);
     return 0;
 }
