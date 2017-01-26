f0e1207a2a0e355b9760bc0511dfee4e36596eb1
Mickaël Bernardini <contact@mickael-bernardini.fr>
2017-01-26 18:04:34 +0100

    Verison sans le bound

--- tp2.c
+++ tp2.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 typedef struct object {
-    int name;
+    int number;
     float weight;
     float value;
     float ratio;
@@ -16,9 +16,7 @@ typedef struct soluce {
 
 int getNbObject(FILE* f) {
     int nb_object = 0;
-    do {
-        if (fgetc(f) == '\n') nb_object ++;
-    } while (! feof(f));
+    while (! feof(f)) if (fgetc(f) == '\n') nb_object++;
     return nb_object;
 }
 
@@ -35,7 +33,7 @@ void fillObjects(FILE* f, object** objects) {
     do {
         fscanf(f, "%f %f", &weight, &value);
         object* obj = (object*) malloc(sizeof(object));
-        obj->name = i;
+        obj->number = i;
         obj->weight = weight;
         obj->value = value;
         obj->ratio = value / weight;
@@ -54,7 +52,7 @@ void sortObjects(object** objects, int nb_object) {
                 objects[i] = objects[j];
                 objects[j] = tmp;
             }
-            --j;
+            j--;
         }
         i++;
     }
@@ -62,23 +60,22 @@ void sortObjects(object** objects, int nb_object) {
 
 void displayObject(object* object) {
     printf("%d\tpoid: %f\tvaleur: %f\tratio: %f\n",
-        object->name, object->weight, object->value, object->ratio);
+        object->number, object->weight, object->value, object->ratio);
 }
 
-float supBound(object** objects, int start, int end, float max_weight) {
+float supBound(object** objects, int start, int end, float current_value, float max_weight) {
     float consomated_weight = 0;
-    float value = 0;
     for (int i = start; i < end; ++i) {
         if (consomated_weight + objects[i]->weight >= max_weight) { // je coupe
-            value += (objects[i]->value * (max_weight - consomated_weight)) / objects[i]->weight;
+            current_value += (objects[i]->value * (max_weight - consomated_weight)) / objects[i]->weight;
             consomated_weight = max_weight;
             break;
         } else { // je prends
-            value += objects[i]->value;
+            current_value += objects[i]->value;
             consomated_weight += objects[i]->weight;
         }
     }
-    return value;
+    return current_value;
 }
 
 //parcourt profondeur
@@ -87,23 +84,22 @@ float supBound(object** objects, int start, int end, float max_weight) {
 float branchAndBound(object** objects, int level, int nb_object, float weight, float current_value, int* nb_node) {
     if (level >= nb_object) return current_value;
     (*nb_node)++;
-    float s1 = -10000;
-    if (weight - objects[level]->weight >= 0) {
-        s1 = branchAndBound(objects, level + 1, nb_object, weight - objects[level]->weight, current_value + objects[level]->value, nb_node);
-    }
+    float right = -1, left = -1, n_weight = weight - objects[level]->weight;
+    if (n_weight >= 0)
+        right = branchAndBound(objects, level + 1, nb_object, n_weight, current_value + objects[level]->value, nb_node);
+    left = branchAndBound(objects, level + 1, nb_object, weight, current_value, nb_node);
 
-    float s2 = -10000;
-    if (1) {
-        s2 = branchAndBound(objects, level + 1, nb_object, weight, current_value, nb_node);
-    }
+    if (right > left) return right;
+    return left;
 
-    if (s1 > s2) return s1;
-    return s2;
 }
 
 // solution = tableau d'objet
 int main(int argc, char const *argv[]) {
-    if (argc != 2) exit(1);
+    if (argc != 2) {
+        printf("usage: %s chemin/du/sac\n", argv[0]);
+        exit(1);
+    }
 
     // REMPLIR --------------------------------------
     FILE* f = fopen(argv[1], "r");
@@ -116,11 +112,11 @@ int main(int argc, char const *argv[]) {
 
     // TRIER (bulle :| ) --------------------------------------
     sortObjects(objects, nb_object);
-    for (int i = 0; i < nb_object; ++i) displayObject(objects[i]);
+    // for (int i = 0; i < nb_object; ++i) displayObject(objects[i]);
 
     // RESOUDRE --------------------------------------
     int nb_node = 0;
-    float bnb = branchAndBound(objects, 0, nb_object, max_weight, 0, &nb_node);
+    float bnb = branchAndBound(objects, 0, nb_object, max_weight, 0,&nb_node);
     printf("optimum: %f node: %d\n", bnb, nb_node);
 
     // FREE --------------------------------------
