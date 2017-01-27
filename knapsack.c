fc744164d58b13e5c26104c8dc03ec5c1d61dc7c
Mickaël Bernardini <contact@mickael-bernardini.fr>
2017-01-27 16:59:00 +0100

    working

--- knapsack.c
+++ knapsack.c
@@ -7,6 +7,7 @@ typedef struct object {
     float weight;
     float value;
     float ratio;
+    int taken;
 } object;
 
 int getNbObject(FILE* f) {
@@ -27,32 +28,33 @@ int getMaxWeight(FILE* f) {
     return max_weight;
 }
 
-void fillObjects(FILE* f, object** objects) {
+void fillObjects(FILE* f, object* objects) {
     float weight, value;
     int i = 0;
     while(1 < fscanf(f, "%f %f", &weight, &value)) {
-        object* obj = (object*) malloc(sizeof(object));
-        obj->number = i;
-        obj->weight = weight;
-        obj->value = value;
-        obj->ratio = value / weight;
-        objects[i] = obj;
+        object o;
+        o.number = i;
+        o.weight = weight;
+        o.value = value;
+        o.ratio = value / weight;
+        o.taken = 0;
+        objects[i] = o;
         ++i;
     }
 }
 
-void swap(object** objects, int i, int j) {
-    object* tmp = objects[i];
+void swap(object* objects, int i, int j) {
+    object tmp = objects[i];
     objects[i] = objects[j];
     objects[j] = tmp;
 }
 
-void pivot(object** objects, int left, int right, int *p) {
+void pivot(object* objects, int left, int right, int *p) {
     int i;
-    float pivot = objects[left]->ratio;
+    float pivot = objects[left].ratio;
     *p = left;
     for (i = left + 1; i <= right; i++) {
-        if (objects[i]->ratio > pivot) {
+        if (objects[i].ratio > pivot) {
             (*p)++;
             if (i != *p) swap(objects, i, *p);
         }
@@ -60,7 +62,7 @@ void pivot(object** objects, int left, int right, int *p) {
     swap(objects, *p, left);
 }
 
-void quickSort(object** objects, int left, int right) {
+void quickSort(object* objects, int left, int right) {
     int p;
     if (left < right) {
         pivot(objects, left, right, &p);
@@ -69,48 +71,72 @@ void quickSort(object** objects, int left, int right) {
     }
 }
 
-void displayObject(object* object) {
-    printf("%d\tpoid: %f\tvaleur: %f\tratio: %f\n",
-        object->number, object->weight, object->value, object->ratio);
+void displayObjects(object* objects, int nb_object, int displaytaken) {
+    int total_o = 0;
+    float total_w = 0, total_v = 0;
+    printf("\nObjet    Poids   Valeur     Ratio\n");
+    printf("---------------------------------\n");
+    for (int i = 0; i < nb_object; ++i) {
+        if (! displaytaken || objects[i].taken){
+            printf("%5d  %7.2f  %7.2f  %7f  %d\n",
+                   objects[i].number + 1,
+                   objects[i].weight,
+                   objects[i].value,
+                   objects[i].ratio,
+                   objects[i].taken);
+            total_o++;
+            total_w += objects[i].weight;
+            total_v += objects[i].value;
+        }
+    }
+    printf("       -------  -------          \n");
+    printf("       %7.2f  %7.2f     Total:%5d objets\n", total_w, total_v, total_o);
 }
 
-float bound(object** objects, int start, int end, float value, float max_weight) {
+float bound(object* objects, int start, int end, float value, float max_weight) {
     float consomated_weight = 0;
     for (int i = start; i < end; ++i) {
-        if (consomated_weight + objects[i]->weight >= max_weight) { // je coupe
-            value += (objects[i]->value * (max_weight - consomated_weight) / objects[i]->weight);
+        if (consomated_weight + objects[i].weight >= max_weight) { // je coupe
+            value += (objects[i].value * (max_weight - consomated_weight) / objects[i].weight);
             consomated_weight = max_weight;
             break;
         } else { // je prends
-            value += objects[i]->value;
-            consomated_weight += objects[i]->weight;
+            value += objects[i].value;
+            consomated_weight += objects[i].weight;
         }
     }
     return value;
 }
 
-float branchAndBound(object** objects, int level, int nb_object, float weight, float value, float *best_value, int *nb_node) {
+float branchAndBound(object* objects, int level, int nb_object, float weight, float value, float *best_value, int *nb_node) {
     (*nb_node)++;
-    if (level >= nb_object || weight <= 0) return value;
-    float left = -1, right = -1, n_weight = weight - objects[level]->weight;
-    if (*best_value <= bound(objects, level, nb_object, value, weight)) {
-        if (n_weight >= 0) {
-            left = branchAndBound(objects, level + 1, nb_object, n_weight, value + objects[level]->value, best_value, nb_node);
-            if (left > *best_value) *best_value = left;
+    if (level >= nb_object || weight <= 0 || *best_value > bound(objects, level, nb_object, value, weight)) return value;
+    float n_weight = weight - objects[level].weight;
+    if (n_weight >= 0) {
+        float left = branchAndBound(objects, level + 1, nb_object, n_weight, value + objects[level].value, best_value, nb_node);
+        if (left > *best_value) {
+            objects[level].taken = 1;
+            printf("PASSE ! ! !\n");
+            *best_value = left;
         }
-        right = branchAndBound(objects, level + 1, nb_object, weight, value, best_value, nb_node);
-        if (right > *best_value) *best_value = right;
+    }
+    float right = branchAndBound(objects, level + 1, nb_object, weight, value, best_value, nb_node);
+    if (right > *best_value) {
+        *best_value = right;
+        objects[level].taken = 0;
     }
     return *best_value;
 }
 
-float knapsack(object** objects, int nb_object, int max_weight) {
+void knapsack(object* objects, int nb_object, int max_weight) {
     int nb_node = 0;
     float best_value = 0;
     float bnb = branchAndBound(objects, 0, nb_object, max_weight, 0, &best_value, &nb_node);
-    printf("optimum: %f\n", bnb);
-    printf("nombre de nœud: %d\n", nb_node);
-    return bnb;
+    printf("\nSac à dos:\n");
+    printf("Optimum : %.2f\n", bnb);
+    printf("Nombre de nœud : %d\n", nb_node);
+    printf("Avec les objets suivants : \n");
+    displayObjects(objects, nb_object, 1);
 }
 
 int main(int argc, char const *argv[]) {
@@ -122,12 +148,12 @@ int main(int argc, char const *argv[]) {
     // REMPLIR --------------------------------------
     FILE* f = fopen(argv[1], "r");
     int nb_object = getNbObject(f);
-    object** objects = (object**) malloc(nb_object * sizeof(object*));
+    object* objects = (object*) malloc(nb_object * sizeof(object));
     int max_weight = getMaxWeight(f);
     fillObjects(f, objects);
     fclose(f);
-    printf("nb_object : %d\n", nb_object);
-    for (int i = 0; i < nb_object; ++i) displayObject(objects[i]);
+    printf("Nombre d'objets : %d\n", nb_object);
+    displayObjects(objects, nb_object, 0);
 
     // TRIER --------------------------------------
     quickSort(objects, 0, nb_object - 1);
@@ -136,7 +162,6 @@ int main(int argc, char const *argv[]) {
     knapsack(objects, nb_object, max_weight);
 
     // FREE --------------------------------------
-    for (int i = 0; i < nb_object; ++i) free(objects[i]);
     free(objects);
     return 0;
 }
